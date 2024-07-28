ceeb08f62523a8e44d04ac2f3905c5a4bda23dfc
Mahbubur Rahman <77193562+mahbubcsebd@users.noreply.github.com>
2024-07-28 11:52:53 +0600

    Update authValidators.js

--- api/validators/authValidators.js
+++ api/validators/authValidators.js
@@ -36,14 +36,14 @@ const registerValidation = [
         .withMessage('Phone is required')
         .isMobilePhone()
         .withMessage('Phone must be valid'),
-    // body('image')
-    //     .custom((value, { req }) => {
-    //         if (!req.file) {
-    //             throw new Error('Image is required');
-    //         }
-    //         return true;
-    //     })
-    //     .withMessage('Image is required'),
+    body('image')
+        .custom((value, { req }) => {
+            if (!req.file) {
+                throw new Error('Image is required');
+            }
+            return true;
+        })
+        .withMessage('Image is required'),
 ];
 
 const loginValidation = [
@@ -92,7 +92,7 @@ const updatePasswordValidation = [
             throw new Error('Confirm password does not match');
         }
         return true;
-    }
+    })
 ];
 
 module.exports = {
